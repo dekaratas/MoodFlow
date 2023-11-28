3381db5f7d85f7067b5945856417d4545830b896
Rich B <richardbaker1993@gmail.com>
2023-11-28 16:00:09 +0000

    feat: 4 tests passing for home screen

--- moodflow/screens/__tests__/home.tests.tsx
+++ moodflow/screens/__tests__/home.tests.tsx
@@ -1,15 +1,29 @@
+import { render } from '@testing-library/react-native';
 import React from 'react';
 import renderer, { ReactTestRendererJSON } from 'react-test-renderer';
 
 import Home from '../Home/Home';
 
 describe(Home, () => {
-  it('should pass', () => {
-    const tree = renderer.create(<Home />).toJSON() as ReactTestRendererJSON[];
-    console.log(Array.isArray(tree));
-    const testTree = Array.from(tree);
-    testTree.forEach((node) => {
-      expect(node.children?.length).toBe(1);
-    });
+  it('renders correctly', () => {
+    const tree = renderer.create(<Home />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should have multiple child components', () => {
+    const tree = renderer.create(<Home />).toJSON() as ReactTestRendererJSON;
+    expect(tree.children?.length).toBeGreaterThan(0);
+  });
+
+  it('renders the quote header text', () => {
+    const { getByText } = render(<Home />);
+    const textElement = getByText('Here is your quote of the day:');
+    expect(textElement).toBeDefined();
+  });
+
+  it('should render the App logo', () => {
+    const { getByTestId } = render(<Home />);
+    const logoImage = getByTestId('Logo');
+    expect(logoImage).toBeDefined();
   });
 });
