04f73db3fcb1e1bb8d9ee79cfc464e7767e310ad
Rich B <richardbaker1993@gmail.com>
2023-11-28 10:29:20 +0000

    attempting svgs for logos again

--- moodflow/screens/Home/Home.tsx
+++ moodflow/screens/Home/Home.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View, Image } from 'react-native';
 
@@ -10,7 +9,6 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Image source={imageSrc} style={styles.logo} />
-      <StatusBar backgroundColor="#FFFFFF" />
       <Text style={styles.homeText}>I am the styled HomeScreen!</Text>
     </View>
   );
