187d8c1be9cc893ac388c6fc82e55321eb73ee95
Rich B <richardbaker1993@gmail.com>
2023-11-28 09:16:37 +0000

    feat: logo Displaying at the top of the screen.

--- moodflow/screens/Home/styles.ts
+++ moodflow/screens/Home/styles.ts
@@ -7,7 +7,7 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     paddingTop: 20,
   },
