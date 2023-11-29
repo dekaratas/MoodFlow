dc3673ce6ea1e9a1cf088ad2ac181e5dc580a47f
Rich B <richardbaker1993@gmail.com>
2023-11-29 11:18:56 +0000

    feat: installed expo/icons.

--- moodflow/App.tsx
+++ moodflow/App.tsx
@@ -1,3 +1,4 @@
+import { Feather } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
@@ -8,10 +9,22 @@ import Settings from './screens/Settings/Settings';
 
 const Tab = createBottomTabNavigator();
 
+const CustomScreenOptions = {
+  tabBarStyle: {
+    width: '95%',
+    backgroundColor: 'black',
+    alignSelf: 'center',
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  tabBarActiveTintColor: '#1EAED7',
+  tabBarInactiveTintColor: 'grey',
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={CustomScreenOptions}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
